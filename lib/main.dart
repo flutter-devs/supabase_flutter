ac26cf68b2b7c439ea27a18deb81d11a4425b935
Prachi Shukla <prachishukla0799@gmail.com>
2023-07-26 18:03:57 +0530

    Final code - fixed

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://rwznhsvtqcfwmhdzuhey.supabase.co',
+    url: 'https://*************.supabase.co',
     anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3em5oc3Z0cWNmd21oZHp1aGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk5NDQ0OTMsImV4cCI6MjAwNTUyMDQ5M30.pxAGDbACLf6raTWBpgs-gq8BKR5fX-SLXu-O20gdtjU',
+        '****************',
   );
 
   await AppPreference().initialAppPreference();
@@ -42,114 +42,3 @@ class App extends StatelessWidget {
     });
   }
 }
-
-// /// Bottom menu widget
-// class Menu extends HookConsumerWidget {
-//   const Menu({super.key});
-//
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final filter = ref.watch(todoListFilter);
-//
-//     int currentIndex() {
-//       switch (filter) {
-//         case TodoListFilter.completed:
-//           return 2;
-//         case TodoListFilter.active:
-//           return 1;
-//         case TodoListFilter.all:
-//           return 0;
-//       }
-//     }
-//
-//     return BottomNavigationBar(
-//       key: bottomNavigationBarKey,
-//       elevation: 0.0,
-//       onTap: (value) {
-//         if (value == 0) ref.read(todoListFilter.notifier).state = TodoListFilter.all;
-//         if (value == 1) ref.read(todoListFilter.notifier).state = TodoListFilter.active;
-//         if (value == 2) ref.read(todoListFilter.notifier).state = TodoListFilter.completed;
-//       },
-//       items: const <BottomNavigationBarItem>[
-//         BottomNavigationBarItem(icon: Icon(Icons.list), label: 'All', tooltip: 'All'),
-//         BottomNavigationBarItem(
-//           icon: Icon(Icons.circle),
-//           label: 'Active',
-//           tooltip: 'Active',
-//         ),
-//         BottomNavigationBarItem(
-//           icon: Icon(Icons.done),
-//           label: 'Completed',
-//           tooltip: 'Completed',
-//         ),
-//       ],
-//       currentIndex: currentIndex(),
-//       selectedItemColor: Colors.amber[800],
-//     );
-//   }
-// }
-//
-// class TodoItem extends HookConsumerWidget {
-//   const TodoItem({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final todo = ref.watch(currentTodo);
-//     final itemFocusNode = useFocusNode();
-//     final itemIsFocused = useIsFocused(itemFocusNode);
-//
-//     final textEditingController = useTextEditingController();
-//     final textFieldFocusNode = useFocusNode();
-//
-//     return Material(
-//       color: Colors.white,
-//       elevation: 6,
-//       child: Focus(
-//         focusNode: itemFocusNode,
-//         onFocusChange: (focused) {
-//           if (focused) {
-//             textEditingController.text = todo.description;
-//           } else {
-//             // Commit changes only when the textfield is unfocused, for performance
-//             ref.read(todoListProvider.notifier).edit(id: todo.id, description: textEditingController.text);
-//           }
-//         },
-//         child: ListTile(
-//           onTap: () {
-//             itemFocusNode.requestFocus();
-//             textFieldFocusNode.requestFocus();
-//           },
-//           leading: Checkbox(
-//             value: todo.completed,
-//             onChanged: (value) => ref.read(todoListProvider.notifier).toggle(todo.id),
-//           ),
-//           title: itemIsFocused
-//               ? TextField(
-//                   autofocus: true,
-//                   focusNode: textFieldFocusNode,
-//                   controller: textEditingController,
-//                 )
-//               : Text(todo.description),
-//         ),
-//       ),
-//     );
-//   }
-// }
-//
-// bool useIsFocused(FocusNode node) {
-//   final isFocused = useState(node.hasFocus);
-//
-//   useEffect(
-//     () {
-//       void listener() {
-//         isFocused.value = node.hasFocus;
-//       }
-//
-//       node.addListener(listener);
-//       return () => node.removeListener(listener);
-//     },
-//     [node],
-//   );
-//
-//   return isFocused.value;
-// }
